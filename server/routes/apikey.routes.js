b83103aa65bf4ace2ab6b09f53049b06169ab275
draconian0428 <jakus.superdev@gmail.com>
2023-07-13 16:15:24 +0900

    login validate function done

--- server/routes/apikey.routes.js
+++ server/routes/apikey.routes.js
@@ -1,5 +1,19 @@
 const express = require('express');
-
 const app = express();
+const db_object = require('../dbconnect');
+
+app.post('/validate', (req, res) => {
+  const { apikey, paraphrase } = req.body;
+
+  db_object.query("SELECT * FROM `api-keys` INNER JOIN customer ON `api-keys`.busid=customer.busid where apikey='"+apikey+"' AND paraphrase='"+paraphrase+"'", (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+    res.json({
+      isValid: result.length !== 0
+    })
+  })
+  
+})
 
 module.exports = app;
